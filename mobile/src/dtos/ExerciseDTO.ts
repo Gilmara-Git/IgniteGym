e967782d8027169d45ca192b91d61302e7ef01b8
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-07-19 08:11:55 -0400

    feat: create Data Transfer Object format for Exercise , History and User

---
+++ mobile/src/dtos/ExerciseDTO.ts
@@ -0,0 +1,11 @@
+export type ExcerciseDTO = {
+    id: string,
+    demo: string,
+    group: string,  
+    name: string, 
+    repetitions: string, 
+    series: string,
+    thumb: string,
+    updated_at: string,
+    
+    }
