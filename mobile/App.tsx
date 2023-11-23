cea12b6918f912b0531501547a7686a15308d27a
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-11-22 21:05:50 -0500

    feat: install onesignal-expo-plugin  +RN-onesignal

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
 import { AuthContextProvider }  from '@contexts/authContext';
+import OneSignal from 'react-native-onesignal';
 
 
 import { THEME } from './src/themes';
@@ -9,7 +10,7 @@ import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 
-
+OneSignal.setAppId('2eff2a6f-6bb6-4542-a1b8-c86e26544305');
 
 export default function App() {
    const [fontsLoaded ] = useFonts({
